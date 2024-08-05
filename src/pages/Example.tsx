98bbaf8cac8c8199b0b41735eed36c146c3ff60e
chadgarlandscg <chadgarlandscg@gmail.com>
2024-08-05 15:44:50 -0600

    Ad Vite and Terraform IaC (#8)

--- src/pages/Example.tsx
+++ src/pages/Example.tsx
@@ -1,12 +1,12 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import { Chart } from "../components/Chart";
-import { data, velocityInFreeFall } from "./data";
+import { velocityInFreeFall } from "./data";
 
 import { useAppSelector, useAppDispatch } from '../state/hooks'
 import { todoAdded, todoToggled } from '../state/appSlice'
 import { loadData } from "../state/side-effects/appThunk";
 
-export function App(props) {
+export function Example() {
     const [inputText, setInputText] = useState("");
     const [todoId, setTodoId] = useState(0);
     const todos = useAppSelector(s => s.app.todos);
@@ -14,6 +14,9 @@ export function App(props) {
     const dispatch = useAppDispatch();
     return <div>
         <h1>Welcome to our website!</h1>
+        <h2 className="text-3xl font-bold underline">
+            Tailwind is here!
+        </h2>
         <button>Click me</button>
         <div style={{width: 1000, height: 500}}>
             <Chart
@@ -29,13 +32,13 @@ export function App(props) {
             setInputText(e.target.value);
             setTodoId(todoId + 1);
         }}/>
-        <button onClick={e => {
+        <button onClick={() => {
             dispatch(todoAdded({id: todoId, text: inputText}));
         }}>Try this!</button>
         {todos.map(todo => <li>
-            {todo.text} <input type="checkbox" checked={todo.completed} onChange={e => dispatch(todoToggled({id: todo.id}))}/>
+            {todo.text} <input type="checkbox" checked={todo.completed} onChange={() => dispatch(todoToggled({id: todo.id}))}/>
         </li>)}
-        <button onClick={e => dispatch(loadData("", ""))}>Load data!</button>
+        <button onClick={() => dispatch(loadData())}>Load data!</button>
         {JSON.stringify(stuff)}
         
     </div>;
--- src/state/side-effects/appThunk.ts
+++ src/state/side-effects/appThunk.ts
@@ -1,9 +1,9 @@
 import axios from "axios"
 import { dataLoaded } from "../appSlice";
-import { RootDispatch, GetRootState, store } from "../store";
+import { RootDispatch } from "../store";
 
-export const loadData = (drilldown: string, measure: string) => {
-    const loadDataThunk = async (dispatch: RootDispatch, getState: GetRootState) => {
+export const loadData = (/*drilldown: string, measure: string*/) => {
+    const loadDataThunk = async (dispatch: RootDispatch, /*getState: GetRootState*/) => {
         const response = await axios.get(`https://datausa.io/api/data?drilldowns=Nation&measures=Population`);
         dispatch(dataLoaded(response.data));
     }
