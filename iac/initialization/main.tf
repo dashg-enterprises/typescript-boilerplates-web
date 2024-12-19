95200b541b7ecae2332b21768eb86e749f87998b
chadgarlandscg <chadgarlandscg@gmail.com>
2024-12-19 00:33:50 -0700

    Lift configurable application values to locals

--- iac/initialization/main.tf
+++ iac/initialization/main.tf
@@ -9,8 +9,13 @@ terraform {
   required_version = ">= 1.2.0"
 }
 
-module "spa_on_aws" {
-  source = "git::https://github.com/dashg-enterprises/cloud-platform.git//modules/spa-website/aws/initialization?ref=main" #"../../cloud-platform/src/modules/spa-website/aws"#"https://github.com/dashg-enterprises/cloud-platform.git//src/modules/aws/spa-website"
+locals {
   hosted_zone_id = "Z03829103FGKVJE6VHSAF"
   domain = "justthespot.io"
+}
+
+module "spa_on_aws" {
+  source = "git::https://github.com/dashg-enterprises/cloud-platform.git//modules/spa-website/aws/initialization?ref=main" #"../../cloud-platform/src/modules/spa-website/aws"#"https://github.com/dashg-enterprises/cloud-platform.git//src/modules/aws/spa-website"
+  hosted_zone_id = local.hosted_zone_id
+  domain = local.domain
 }
--- iac/spa-website.tf
+++ iac/spa-website.tf
@@ -9,12 +9,18 @@ terraform {
   required_version = ">= 1.2.0"
 }
 
+locals {
+  application_name = "just-the-spot"
+  hosted_zone_id = "Z03829103FGKVJE6VHSAF"
+  domain = "justthespot.io"
+}
+
 module "spa_on_aws" {
   source = "git::https://github.com/dashg-enterprises/cloud-platform.git//modules/spa-website/aws?ref=main" #"../../cloud-platform/src/modules/spa-website/aws"#"https://github.com/dashg-enterprises/cloud-platform.git//src/modules/aws/spa-website"
   dist_folder_path = "../dist"
-  application_name = "just-the-spot-${var.environment}"
-  hosted_zone_id = "Z03829103FGKVJE6VHSAF"
-  domain = "justthespot.io"
+  application_name = "${local.application_name}-${var.environment}"
+  hosted_zone_id = local.hosted_zone_id
+  domain = local.domain
   subdomain = var.environment_subdomain
   build_id = var.build_id
   cert_arn = var.cert_arn
