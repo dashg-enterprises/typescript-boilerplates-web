26f783f11615e34c0cfa53b82489196bcfe71d77
chadgarlandscg <chadgarlandscg@gmail.com>
2024-12-21 21:37:47 -0700

    Add build bucket to project initialization outputs

--- iac/initialization/outputs.tf
+++ iac/initialization/outputs.tf
@@ -2,3 +2,13 @@ output "cert_arn" {
   description = "The cert created for the domain"
   value = module.spa_on_aws.cert_arn
 }
+
+output "build_bucket" {
+  description = "Bucket used for SPA builds"
+  value = module.spa_on_aws.build_bucket
+}
+
+output "build_bucket_id" {
+  description = "Bucket id used for SPA builds"
+  value = module.spa_on_aws.build_bucket_id
+}
