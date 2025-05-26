7dddd265da76a9964d3ac9968eb7a77a639245d7
Brandy Garland <brandy.e.garland@gmail.com>
2025-05-26 01:45:19 -0600

    Remove terraform provider from iac

--- iac/spa-website.tf
+++ iac/spa-website.tf
@@ -1,14 +1,3 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.59.0"
-    }
-  }
-
-  required_version = ">= 1.2.0"
-}
-
 locals {
   application_name = "just-the-spot"
   hosted_zone_id = "Z03829103FGKVJE6VHSAF"
